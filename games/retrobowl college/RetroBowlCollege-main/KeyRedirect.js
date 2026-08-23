43a19c76d6849e2287b34bdbef7eabaf556ec323
TBEMath <144234765+tonic120@users.noreply.github.com>
2026-08-23 02:23:27 -0500

    Delete games/retrobowl college directory

--- games/retrobowl college/RetroBowlCollege-main/KeyRedirect.js
+++
@@ -1,5 +0,0 @@
-document.addEventListener('keydown', function(event) {
-    if (event.key === '`') { // Check if the key pressed is the backtick
-        window.location.href = 'https://www.google.com'; // Redirect to Google
-    }
-});
